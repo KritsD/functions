22d0d64996c0faf59de7c173eebd25af7ae391af
KritsD <143739822+KritsD@users.noreply.github.com>
2024-03-30 18:26:51 -0400

    Styling + JSON

--- data.js
+++ data.js
@@ -6,18 +6,10 @@ const renderItems = (data) => {
 
 	// Loop through each item in the data array
 	data.availableTime.forEach((item) => {
-		//let conditionalClass = '' // Set an empty class variable
-
-		//if (!item.alsoWasWriter) { // Conditional if this is `false` (“not true”)
-		//	conditionalClass = 'faded' // Update the variable
-		//}
-
-		// Make a “template literal” as we have before, inserting your data (and maybe the class)
 		let listItem =
 			`
                 <option value = "${item.value}">${item.text}</option>
 			`
-
 		dataList.insertAdjacentHTML('beforeend', listItem) // Add it to the `ul`!
 	})
 
@@ -34,7 +26,6 @@ const renderItems = (data) => {
 			`
                 <option value = "${item.value}">${item.text}</option>
 			`
-
 		dataList2.insertAdjacentHTML('beforeend', listItem) // Add it to the `ul`!
 	})
 
@@ -56,20 +47,38 @@ function outputSolution(){
                     results.classList.add("HeadacheClass");
                     result = "Close eyes and breathe";
                     break;
-                case "Sore Eyes":
-                    result = "Blink Slowly";
-                    break;    
+                case "Sore eyes":
+                    result = "Try these exercises";
+                    break;  
+				case "Blurred vision":
+                    result = "Follow the 20-20-20";
+                    break;
+				case "Shoulder/neck pain":
+					result = "Neck and shoulder rolls";
+					break;  
+				case "Fatigue":
+					result = "Get off screen and rest your eyes!";
+					break; 
             }
         case "I have some time":
             switch (dataList2)
             {
                 case "Headache":
-                    results.classList.add("HeadacheClass");
+                    results.classList.add("SometimeClass");
                     result = "Close eyes and breathe";
                     break;
-                case "Sore Eyes":
+                case "Sore eyes":
                     result = "Blink Slowly";
-                    break;    
+                    break;  
+				case "Blurred vision":
+					result = "Use eye drops";
+					break;
+				case "Shoulder/neck pain":
+					result = "Neck and shoulder rolls";
+					break;  
+				case "Fatigue":
+					result = "Get off screen and rest your eyes!";
+					break;   
             }
         
     }
