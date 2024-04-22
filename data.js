d57e1fedfcaed7d168b2aba10f99f9097f008ca6
KritsD <143739822+KritsD@users.noreply.github.com>
2024-04-22 17:29:27 -0400

    Lotsof Edits!!!!

--- data.js
+++ data.js
@@ -59,37 +59,37 @@ function outputSolution(){
             switch (dataList2) {
                 case "Headache":
 					result = "<span class='p2'> Point your indoor light source, like a lamp, away from your eyes. To reduce screen glare, make sure the light source shines away from your computer screen. On digital screens, adjust the brightness to match the level of light around you. Also, adjust the screen's contrast, so your eyes don't have to strain to see.</span>";
-					resultsfeelingHeader.innerHTML = "symptom: <br> HEADACHE";
-					resultstimeHeader.innerHTML = "time:<br> A MINUTE";
-					resultsremedyHeader.innerHTML = "remedy:<br> USE PROPER LIGHTING";
+					resultsfeelingHeader.innerHTML = "<span class='p3'>symptom:</span> <br> HEADACHE";
+					resultstimeHeader.innerHTML = "<span class='p3'>time:</span><br> A MINUTE";
+					resultsremedyHeader.innerHTML = "<span class='p3'>remedy:</span><br> USE PROPER LIGHTING";
 					break;
 				case "Sore eyes":
 					// result = "<span class='p2'>Palming your eyes has two important effects: first, it requires that your eyes are closed for a while, which is good for eye strain on its own. Second, some studies suggest that it may help your eyes circulate blood and liquid better, helping your eyes recover from hard work more easily.</span>";
 					result ="<span class='p2'>For 2 minutes, gently cup your palms and place them over your eyes. Make sure you can open and close your eyes freely. Staring and blinking into the darkness can help your vision reset.</span>";
-					resultsfeelingHeader.innerHTML = "symptom:<br> SORE EYES";
-					resultstimeHeader.innerHTML = "time:<br> A MINUTE";
-					resultsremedyHeader.innerHTML = "remedy:<br>PALM THE EYES";
+					resultsfeelingHeader.innerHTML = "<span class='p3'>symptom:</span><br> SORE EYES";
+					resultstimeHeader.innerHTML = "<span class='p3'>time:</span><br> A MINUTE";
+					resultsremedyHeader.innerHTML = "<span class='p3'>remedy:</span><br>PALM THE EYES";
 					break;  
 				case "Blurred vision":
 					result = "<span class='p2'>Practicing taking breaks by looking away from the screen can help reduce eye tension and pain.</span>";
 					result += "<span class='p2'>Look at something at least 20 feet away for 20 seconds every 20 minutes.</span>";
-					resultsfeelingHeader.innerHTML = "symptom: <br> BLURED VISION";
-					resultstimeHeader.innerHTML = "time:<br> A MINUTE";
-					resultsremedyHeader.innerHTML = "remedy:<br> FOLLOW 20-20-20 RULE";
+					resultsfeelingHeader.innerHTML = "<span class='p3'>symptom:</span> <br> BLURED VISION";
+					resultstimeHeader.innerHTML = "<span class='p3'>time:</span><br> A MINUTE";
+					resultsremedyHeader.innerHTML = "<span class='p3'>remedy:</span><br> FOLLOW 20-20-20 RULE";
                     break;
 				case "Shoulder/neck pain":
 					result = "<span class='p2'>Drop your head gently to your chest. While breathing in, slowly roll your head up to your left shoulder. While breathing out, slowly roll your head back to center. Repeat to the right. Repeat 3 times on each side.</span>";
 					result += "<span class='p2'>Head tilt: Sit up straight. Tuck in your chin. Slowly tip your head to the left. Return to the center. Then, tip your head to the right.Repeat 3 times on each side.</span>";
-					resultsfeelingHeader.innerHTML = "symptom: <br> SHOULDER/NECK PAIN";
-					resultstimeHeader.innerHTML = "time: <br>A MINUTE";
-					resultsremedyHeader.innerHTML = "remedy:<br> NECK & SHOULDER ROLLS";
+					resultsfeelingHeader.innerHTML = "<span class='p3'>symptom: </span><br> SHOULDER/NECK PAIN";
+					resultstimeHeader.innerHTML = "<span class='p3'>time: </span><br>A MINUTE";
+					resultsremedyHeader.innerHTML = "<span class='p3'>remedy:</span><br> NECK & SHOULDER ROLLS";
 					break;  
 				case "Eye Fatigue":
 					// result ="<span class='p2'>Flexing is an eye exercise that stretches and strengthens the occular muscles in the eye. This involves rapid movements up and down, then left and right.</span>";
 					result ="<span class='p2'>Face forward and look straight ahead. Look up without moving your head and then look down. Repeat this motion 10 times.</span>";
-					resultsfeelingHeader.innerHTML = "symptom:<br>EYE FATIGUE";
-					resultstimeHeader.innerHTML = "time: <br>A MINUTE";
-					resultsremedyHeader.innerHTML = "remedy:<br> FLEX THE EYES";
+					resultsfeelingHeader.innerHTML = "<span class='p3'>symptom:</span><br>EYE FATIGUE";
+					resultstimeHeader.innerHTML = "<span class='p3'>time: </span><br>A MINUTE";
+					resultsremedyHeader.innerHTML = "<span class='p3'>remedy:</span><br> FLEX THE EYES";
 					break; 
             }
 			break;
@@ -98,34 +98,34 @@ function outputSolution(){
         	switch (dataList2){
                 case "Headache":
                     result = "<span class='p2'>Close your eyes and take 15 long deep breaths</span>";
-					resultsfeelingHeader.innerHTML = "symptom: <br> HEADACHE";
-					resultstimeHeader.innerHTML = "time: <br>SOME TIME";
-					resultsremedyHeader.innerHTML = "remedy: <br>CLOSE EYES & BREATHE";
+					resultsfeelingHeader.innerHTML = "<span class='p3'>symptom:</span> <br> HEADACHE";
+					resultstimeHeader.innerHTML = "<span class='p3'>time:</span> <br>SOME TIME";
+					resultsremedyHeader.innerHTML = "<span class='p3'>remedy:</span> <br>CLOSE EYES & BREATHE";
                     break;
                 case "Sore eyes":
 					// result ="<span class='p2'>The warmth from the compress helps trigger tears while relaxing muscles. This helps relieve eye strain without putting any additional stress on delicate muscles.</span>";
 					result ="<span class='p2'>Take a washcloth soaked in warm water. Wash your hands before you start so bacteria doesn't get in your eyes. Dip your washcloth in water that you've boiled (to sterilize it) and cooled until lukewarm. Then put it on your eyes for 5-10 minutes, or as long as your doctor advises.</span>";
-					resultsfeelingHeader.innerHTML = "symptom:<br> SORE EYES";
-					resultstimeHeader.innerHTML = "time:<br> SOME TIME";
-					resultsremedyHeader.innerHTML = "remedy: <br>A WARM COMPRESS";
+					resultsfeelingHeader.innerHTML = "<span class='p3'>symptom:</span><br> SORE EYES";
+					resultstimeHeader.innerHTML = "<span class='p3'>time:</span><br> SOME TIME";
+					resultsremedyHeader.innerHTML = "<span class='p3'>remedy: </span><br>A WARM COMPRESS";
                     break;  
 				case "Blurred vision":
 					result = "<span class='p2'>After you've brewed chamomile or regular tea, put the bags in a clean container in the fridge. Once they've cooled, put them on your eyelids to help soothe your eyes and reduce swelling. Make sure you've washed your face and hands well and taken out your contacts. And don't get any tea in your eyes.</span>";
-					resultsfeelingHeader.innerHTML = "symptom: <br> BLURED VISION";
-					resultstimeHeader.innerHTML = "time:<br> SOME TIME";
-					resultsremedyHeader.innerHTML = "remedy:<br> COOLED CHAMOMILE BAGS";
+					resultsfeelingHeader.innerHTML = "<span class='p3'>symptom:</span> <br> BLURED VISION";
+					resultstimeHeader.innerHTML = "<span class='p3'>time:</span><br> SOME TIME";
+					resultsremedyHeader.innerHTML = "<span class='p3'>remedy:</span><br> COOLED CHAMOMILE BAGS";
 					break;
 				case "Shoulder/neck pain":
 					result = "<span class='p2'>Make sure your screen is about arm's length (20-26 inches) away from your face. The center of the screen should be slightly below eye level (4-5 inches). It helps to have a chair you can move up and down Adjust the text size so you don't squint.</span>";
-					resultsfeelingHeader.innerHTML = "symptom: <br> SHOULDER/NECK PAIN";
-					resultstimeHeader.innerHTML = "time: <br>SOME TIME";
-					resultsremedyHeader.innerHTML = "remedy:<br>ADJUST YOUR SCREEN";
+					resultsfeelingHeader.innerHTML = "<span class='p3'>symptom:</span> <br> SHOULDER/NECK PAIN";
+					resultstimeHeader.innerHTML = "<span class='p3'>time:</span> <br>SOME TIME";
+					resultsremedyHeader.innerHTML = "<span class='p3'>remedy:</span><br>ADJUST YOUR SCREEN";
 					break;  
 				case "Eye Fatigue":
 					result = "<span class='p2'>Take a screen break, rest your eyes for several minutes</span>";
-					resultsfeelingHeader.innerHTML = "symptom:<br>EYE FATIGUE";
-					resultstimeHeader.innerHTML = "time:<br> SOME TIME";
-					resultsremedyHeader.innerHTML = "remedy:<br>REST";
+					resultsfeelingHeader.innerHTML = "<span class='p3'>symptom:</span><br>EYE FATIGUE";
+					resultstimeHeader.innerHTML = "<span class='p3'>time:</span><br> SOME TIME";
+					resultsremedyHeader.innerHTML = "<span class='p3'>remedy:</span><br>REST";
 					break;   
             	}
 				break;
